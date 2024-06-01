97ea5aa4f9ecee7baabce7ed2134aedc3500a917
Ankit Lodhi <ankitlidhoriya2002@gmail.com>
2024-06-01 17:04:55 +0530

    Node http intro

---
+++ Node-2  31 may 2024 HTTP & HTTPS/example1/index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const server = http.createServer((req, res)=>{
+    if(req.url==="/Data"){
+        console.log("Random Page");
+        res.end("Hello, Random Page")
+    }else if(req.url==="/About"){
+        console.log("About Page");
+        res.end("Hello, About Page")
+    }else if(req.url==="/Home"){
+        console.log("Home Page")
+        res.end("Hello, Home page ")
+    }else if(req.url==="/Profile"){
+        console.log("Profile page")
+        res.end("Hello, Profile Page")
+    }
+})
+
+server.listen(3000,()=>{
+    console.log("Runnig");
+})
